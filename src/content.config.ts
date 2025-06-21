18459c4bec9155eaab049617845e5fb7a434b433
probablyduncan <duncanpetrie1@gmail.com>
2025-06-21 14:18:12 +0100

    save progress

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,51 @@
 import { file } from "astro/loaders";
-import { defineCollection, reference, z } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
-const links = defineCollection({
-    loader: file("src/content/links.yaml"),
+const minDate = new Date(0);
+const resolveDate = (d: string | undefined) => {
+    if (!d) {
+        return undefined;
+    }
+
+    const parsed = new Date(d);
+    if (isNaN(parsed.valueOf())) {
+        return undefined;
+    }
+
+    return parsed;
+}
+
+const weblinks = defineCollection({
+    loader: file("src/content/weblinks.yaml"),
     schema: z.object({
         title: z.string(),
-        href: z.string(),
+        href: z.string().transform(href => href.endsWith("/") ? href.substring(0, href.length - 1) : href),
+        // href: z.string().transform(href => href.endsWith("/") ? href : href + "/"),
         desc: z.string(),
         date: z.string(),
-        tags: z.array(z.string()).optional(),
+        sortDate: z.string().optional(),
+        tags: z.array(z.string())
+            .or(z.string().transform((tag: string) => [tag])),
+    }).transform(link => {
+        return {
+            title: link.title,
+            href: link.href,
+            desc: link.desc,
+            tags: link.tags,
+            date: {
+                text: link.date,
+                obj: resolveDate(link.sortDate) ?? resolveDate(link.date) ?? minDate,
+            },
+        };
     }),
 });
 
-export const collections = { links };
+const ticker = defineCollection({
+    loader: file("src/content/ticker.yaml"),
+    schema: z.object({
+        href: z.string(),
+        text: z.string(),
+    }),
+})
+
+export const collections = { weblinks, ticker };
---
+++ src/pages/colophon.md
@@ -0,0 +1,5 @@
+the ticker is a fun bit of tech
+
+fonts
+
+css-only filtering
