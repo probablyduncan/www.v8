6001723f067a3d9a94feaad6c4c74713cdd851e1
probablyduncan <duncanpetrie1@gmail.com>
2025-07-18 13:06:57 +0100

    default selected filters

--- src/lib/filterableHelper.ts
+++ src/lib/filterableHelper.ts
@@ -2,6 +2,11 @@ export interface FilterButtonProps {
     listName: string;
     tags: string[];
     showAll?: "off" | "first" | "last";
+    
+    /**
+     * Tag that will be selected by default. If not set, the first tag (or all) will be the default.
+     */
+    defaultTag?: string;
 }
 
 export function getFilterableTagClass(listName: string, tag?: string) {
