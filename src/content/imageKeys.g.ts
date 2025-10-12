ca9a03c4d0119ffefdf4b9a5ab5027fe1f73fe9b
probablyduncan <duncanpetrie1@gmail.com>
2025-10-12 12:12:49 +0100

    image intake for offline, add images

--- src/content/imageKeys.g.ts
+++ src/content/imageKeys.g.ts
@@ -1,5 +1,5 @@
 //GENERATED CODE - MODIFICATIONS WILL BE OVERWRITTEN
 
-export const IMAGE_NAMES = ["icewater_lake_michigan-2017-01-20", "rainshadow_door-2020-07-06", "2020-07-26-DSC_0216.g.avif", "gloaming_keweenaw_peninsula-2020-08-05", "mist_water_pennance_point-2020-11-28", "film_blur_falmouth", "bigmapW4_small.jpg", "glitch-screenshot-3.webp", "wiki.gif", "pizza.gif"] as const;
+export const IMAGE_NAMES = ["icewater_lake_michigan-2017-01-20", "rainshadow_door-2020-07-06", "2020-07-26-DSC_0216.g.avif", "gloaming_keweenaw_peninsula-2020-08-05", "mist_water_pennance_point-2020-11-28", "chimneys-2025-04-14", "film_blur_falmouth", "sunblind-2025-09-01", "sunblind_from_ferry-2025-09-02", "bigmapW4_small.jpg", "glitch-screenshot-3.webp", "pizza.gif", "wiki.gif"] as const;
 
-export const IMAGE_TAGS = ["site-best", "story - stillwater", "_photos that shoud have names", "site-all", "milwaukee", "wisconsin", "usa", "abstracts all", "abstracts", "bookmaybeidk", "site-horizons", "oldhoriz", "world", "door county", "keweenaw", "michigan", "huw", "falmouth", "cornwall", "uk", "kintzing"] as const;
+export const IMAGE_TAGS = ["site-best", "story - stillwater", "_photos that shoud have names", "site-all", "milwaukee", "wisconsin", "usa", "abstracts all", "abstracts", "bookmaybeidk", "site-horizons", "oldhoriz", "world", "door county", "keweenaw", "michigan", "huw", "falmouth", "cornwall", "uk", "kintzing", "london", "washington island"] as const;
